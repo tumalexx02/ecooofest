56aae9173b041bc9e85828beb286bf234790561c
Aleksandr Tumanov <tumalexx02@gmail.com>
2023-09-28 21:03:46 +0500

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ECO-FEST LANDING PAGE
+My first HTML / SCSS / JS landing page project, it was created for ECO-FEST case championship. After taking courses at Udemy on basic web development, I decided to try to implement the first project. After that, my first customer wrote to me and asked for help in creating a landing page, and my first project appeared.
+<br><br>
+In this project, I tried to hone my HTML and CSS layout skills, also using the SCSS preprocessor for more convenient styling. The layout was done according to the BEM methodology. There was also a goal to get acquainted with JavaScript at the initial level by adding simple interactive elements.
+<br><br>
+The customer was satisfied with the work, and I fulfilled my goals ✌(ツ)
+
+# LINK TO LANDING PAGE
+https://ecooofest.com
+<br><br>
+![alt text](./dist/img/for_readme.png)
---
+++ js/script.js
@@ -0,0 +1,109 @@
+import { ruPage, enPage } from "./pages.js";
+
+// WOW initialization
+new WOW().init();
+onhashchange = e => {
+    history.replaceState(null,"", e.oldURL);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (!localStorage.getItem('lang')) {
+    if (navigator.language === 'ru-RU' || navigator.language === 'ru') {
+      localStorage.setItem('lang', 'ru')
+      location.reload()
+    } else {
+      localStorage.setItem('lang', 'en')
+      location.reload()
+    }
+  }
+
+  if (localStorage.getItem('lang') === 'ru') {
+    document.querySelector('.page').innerHTML = ruPage
+  } else {
+    document.querySelector('.page').innerHTML = enPage
+  }
+
+  const hamburgerIcon = document.querySelector('.hamburger__icon');
+  const hamburgerElement = document.querySelector('.hamburger');
+
+  // Toggle hamburger_active class when hamburger button is clicked
+  document.querySelector('.hamburger__icon').addEventListener('click', () => {
+    hamburgerElement.classList.toggle('hamburger_active');
+  });
+
+  // Removing hamburger active class when background behind hamburger is clicked
+  const hamburgerBg = document.querySelector('.hamburger__background');
+
+  hamburgerBg.addEventListener('click', function() {
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.remove('hamburger_active');
+  });
+
+  // Removing active class when background behind modal window is clicked
+  const modalBg = document.querySelector('.modal__bg');
+
+  modalBg.addEventListener('click', function() {
+    const modal = document.querySelector('.modal');
+    const mateCardMobile = document.querySelector('.mate-card_modal');
+
+    modal.classList.remove('modal_active');
+    mateCardMobile.classList.remove('mate-card_modal_active')
+  });
+
+  // Showing modal window when any button_modal is clicked
+  const buttonModal = document.querySelectorAll('.button_modal');
+
+  buttonModal.forEach(button => {
+    button.addEventListener('click', function() {
+      const modal = document.querySelector('.modal');
+      const mateCardMobile = document.querySelector('.mate-card_modal');
+
+      modal.classList.toggle('modal_active');
+      mateCardMobile.classList.toggle('mate-card_modal_active')
+    });
+  });
+
+  // Hiding modal window when any modal__close is clicked
+  const closeModal = document.querySelector('.modal__close');
+
+  closeModal.addEventListener('click', function() {
+    const modal = document.querySelector('.modal');
+    const mateCardMobile = document.querySelector('.mate-card_modal');
+
+    modal.classList.remove('modal_active');
+    mateCardMobile.classList.remove('mate-card_modal_active')
+  });
+
+  // Removing hamburber active class when link in hamburger is clicked
+  document.querySelectorAll('.hamburger__link').forEach(link => {
+    link.addEventListener('click', hamburgerElement.classList.remove('hamburger_active'));
+  });
+
+  // Upper show/hide script
+  window.addEventListener('scroll', function() {
+    const upper = document.querySelector('.upper__link')
+    if (window.scrollY > 500) {
+      upper.classList.add('upper__link_active');
+    } else {
+      upper.classList.remove('upper__link_active');
+    }
+  });
+
+  // Language toggling
+  const languageButtons = document.querySelectorAll('.language-link');
+
+  languageButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (button.innerHTML === 'English') {
+        localStorage.removeItem('lang');
+        localStorage.setItem('lang', 'en');
+        location.reload()
+      } else if (button.innerHTML === 'Русский') {
+        localStorage.removeItem('lang');
+        localStorage.setItem('lang', 'ru');
+        location.reload();
+      }
+    })
+  })
+});
